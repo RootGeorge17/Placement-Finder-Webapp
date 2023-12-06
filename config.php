ed4542fc601ea8e0334268affa66a101fbb1900c
mujahid <mujahid9676@gmail.com>
2023-12-06 15:37:37 +0000

    datasets refactored, display cards implemented

--- config.php
+++ config.php
@@ -2,8 +2,10 @@
 
 return [
     'database' => [
-        'host' => '', // Database host
-        'dbname' => '', // Database name
+        'host' => 'poseidon.salford.ac.uk', // Database host
+        'dbname' => 'hc23_17_hackcamp', // Database name
         'charset' => 'utf8mb4' // Character set for the database connection
     ],
+    'username' => 'hc23-17',
+    'password' => 'LOanukKp2py35Mm'
 ];
