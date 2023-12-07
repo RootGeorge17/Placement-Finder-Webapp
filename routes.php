a0bf245d8c993e425510312eff65f663de8274c9
FMir3 <f.mir3@edu.salford.ac.uk>
2023-12-07 12:28:15 +0000

    register frontend mostly completed

--- routes.php
+++ routes.php
@@ -5,3 +5,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/dashboard', 'controllers/studentdashboard.php');
 
 $router->get('/register', 'controllers/Authentication/register.php');
+
+$router->post('/register', 'controllers/Authentication/register.php');
+
+$router->get('/register', 'controllers/Authentication/register/register.php');
