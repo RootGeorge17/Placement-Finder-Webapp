1e95c5a257050199ba739e628bde40cd2aa0fa66
Muj <muji2498@users.noreply.github.com>
2023-12-07 14:17:10 +0000

    dashboard.php fixed

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,18 +1,23 @@
 <?php
 
+require(base_path("models/DataSets/PlacementsDataSet.php"));
+require(base_path("models/DataSets/StudentsDataSet.php"));
+require(base_path("models/DataSets/SkillsDataSet.php"));
+require(base_path("models/DataSets/CompaniesDataSet.php"));
+require(base_path("models/Extensions/PlacementHelpers.php"));
+
 if($_SESSION['user']['usertype'] == 1)
 {
-    require(base_path("models/DataSets/ProficienciesDataSet.php"));
-    require(base_path("models/Extensions/PlacementHelpers.php"));
+    require_once base_path("models/DataSets/ProficienciesDataSet.php");
 
 
     $placementsDataSet = new PlacementsDataSet();
     $studentsDataSet = new StudentsDataSet();
     $skillsDataSet = new SkillsDataSet();
     $companiesDataSet = new CompaniesDataSet();
+    $placementHelpers = new PlacementHelpers();
     $proficiencies = new ProficienciesDataSet();
 
-    $placementHelpers = new PlacementHelpers();
 
     $allProficiencies = $proficiencies->fetchAllProficiencies();
     $allPlacements = $placementsDataSet->fetchAllPlacements();
@@ -36,16 +41,11 @@ if($_SESSION['user']['usertype'] == 1)
 
 } elseif($_SESSION['user']['usertype'] == 2)
 {
-    require(base_path("models/DataSets/PlacementsDataSet.php"));
-    require(base_path("models/DataSets/CompaniesDataSet.php"));
-    require(base_path("models/DataSets/SkillsDataSet.php"));
-    require(base_path("models/DataSets/StudentsDataSet.php"));
-    require(base_path("models/Extensions/PlacementHelpers.php"));
 
     $placementsDataSet = new PlacementsDataSet();
-    $companiesDataSet = new CompaniesDataSet();
-    $skillsDataSet = new SkillsDataSet();
     $studentsDataSet = new StudentsDataSet();
+    $skillsDataSet = new SkillsDataSet();
+    $companiesDataSet = new CompaniesDataSet();
     $placementHelpers = new PlacementHelpers();
     $allStudents = $studentsDataSet->fetchAllStudentData();
 
--- routes.php
+++ routes.php
@@ -9,4 +9,3 @@ $router->post('/login', 'controllers/Authentication/Login/store.php');
 
 $router->get('/register', 'controllers/Authentication/register/register.php');
 $router->post('/register', 'controllers/Authentication/register/register.php');
-
