cbe63d3ce9aeb0fa555d25dfd79c203a6e672fc2
RootGeorge <g.sopov@edu.salford.ac.uk>
2023-12-09 21:02:31 +0000

    register halfway done

--- controllers/Authentication/register/store.php
+++ controllers/Authentication/register/store.php
@@ -1,11 +1,11 @@
 <?php
 
 use models\Core\Validator;
+
 require_once(base_path("models/DataSets/UsersDataSet.php"));
 $usersDataSet = new UsersDataSet();
 
-if($_POST['submit'] == "first")
-{
+if ($_POST['submit'] == "first") {
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $accountType = $_POST['accountType'];
@@ -13,34 +13,56 @@ if($_POST['submit'] == "first")
     $password = $_POST['password'];
     $confirmPassword = $_POST['confirmPassword'];
 
-    if(!Validator::string($firstName, 2, 75) && !Validator::string($lastName, 2, 75))
-    {
-        $errors['InvalidName'] = "First and Last names must be between 2 and 75 characters maximum!";
+    if (!Validator::string($firstName, 2, 75) && !Validator::string($lastName, 2, 75)) {
+        $errors['InvalidName'] = "First name must be between 2 and 75 characters maximum!";
     }
 
-    if(!Validator::email($email))
-    {
+    if (!Validator::string($lastName, 2, 75)) {
+        $errors['InvalidLastName'] = "Last name must be between 2 and 75 characters maximum!";
+    }
+
+    if (!Validator::email($email)) {
         $errors['InvalidEmail'] = "You have provided an invalid email";
-    }
+    } elseif (Validator::email($email)) // and check for email existance) {
+        $errors['InvalidEmail'] = "The email provided is associated with an existing account!";
 
-    if(!Validator::string($password, 2, 75))
-    {
+    if (!Validator::string($password, 2, 75)) {
         $errors['InvalidPassword'] = "Password must be between 2 and 75 characters maximum!";
     }
 
-    if($password !== $confirmPassword )
-    {
+    if ($password !== $confirmPassword) {
         $errors['PasswordMatch'] = "Passwords do not match!";
     }
 
-    if($accountType == "student")
-    {
-        header('Location: /register?step=2');
+    if (empty($errors)) {
+        if ($accountType == "student") {
+            header('Location: /register?step=2');
+            exit();
+        }
+
+        if ($accountType == "employer") {
+            header('Location: /register?step=3');
+            exit();
+        }
+
+    } else {
+        return view('Authentication/register.phtml', [
+            'errors' => $errors,
+            'pageTitle' => 'Registration'
+        ]);
+    }
+}
+
+if ($_POST['submit'] == "second") {
+
+
+
+    if (empty($errors)) {
+        header('Location: /dashboard');
         exit();
     }
-
-    header('Location: /register?step=3');
 }
+
 
 
 
---
+++ models/Extensions/GenerateStudentFormData.php
@@ -0,0 +1,51 @@
+<?php
+
+class GenerateStudentFormData
+{
+    protected $courses, $institutions, $skills, $proficiencies;
+
+    // Getter and Setter for $courses
+    public function getCourses()
+    {
+        $this->setCourses();
+        return $this->courses;
+    }
+
+    public function setCourses()
+    {
+        // generate courses and set them to this->courses
+    }
+
+    // Getter and Setter for $institutions
+    public function getInstitutions()
+    {
+
+    }
+
+    public function setInstitutions()
+    {
+
+    }
+
+    // Getter and Setter for $skills
+    public function getSkills()
+    {
+
+    }
+
+    public function setSkills()
+    {
+
+    }
+
+    // Getter and Setter for $proficiencies
+    public function getProficiencies()
+    {
+
+    }
+
+    public function setProficiencies()
+    {
+
+    }
+}
