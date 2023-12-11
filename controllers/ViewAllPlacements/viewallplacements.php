f87db4f57d15d796de54fedf12f023e83ef31230
Muj <muji2498@users.noreply.github.com>
2023-12-11 21:06:27 +0000

    view placements mostly done

--- controllers/ViewAllPlacements/viewallplacements.php
+++ controllers/ViewAllPlacements/viewallplacements.php
@@ -9,6 +9,7 @@ $placementsDataSet = new PlacementsDataSet();
 $companiesDataSet = new CompaniesDataSet();
 $industriesDataSet = new IndustriesDataSet();
 $placementHelpers = new PlacementHelpers();
+$proficienciesDataSet = new ProficienciesDataSet();
 $skillsDataSet = new SkillsDataSet();
 
 view('/ViewAllPlacements/viewallplacements.phtml', [
@@ -17,6 +18,7 @@ view('/ViewAllPlacements/viewallplacements.phtml', [
         'companiesDataSet' => $companiesDataSet,
         'industriesDataSet' => $industriesDataSet,
         'skillsDataSet' => $skillsDataSet,
+        'allProficiencies' => $proficienciesDataSet->fetchAllProficiencies(),
         'placementHelpers' => $placementHelpers,
         ]
 );
