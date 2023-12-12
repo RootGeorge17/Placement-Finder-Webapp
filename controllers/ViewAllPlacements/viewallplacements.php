3b93b7015f7b33bec5dbbffba28ad46d9dc3461c
Muj <muji2498@users.noreply.github.com>
2023-12-12 12:09:27 +0000

    pagination done for placements
    TODO: needs dropdown fixing

--- controllers/ViewAllPlacements/viewallplacements.php
+++ controllers/ViewAllPlacements/viewallplacements.php
@@ -12,13 +12,43 @@ $placementHelpers = new PlacementHelpers();
 $proficienciesDataSet = new ProficienciesDataSet();
 $skillsDataSet = new SkillsDataSet();
 
+$limit = 2; // default limit of deliveries per page
+$page = 1; // default page number
+if (isset($_GET['limit'])) {
+    if ($_GET['limit'] >= 1 && $_GET['limit'] <= 64) {
+        $limit = $_GET['limit'];
+    }
+    else {
+        header("Location: /placements?page=1&limit=16");
+    }
+}
+
+if (isset($_GET['page'])) {
+    if ($_GET['page'] >= 1) {
+        $page = $_GET['page'];
+    }
+    else {
+        header("Location: /placements?page=1&limit=16");
+    }
+}
+
+$start = ($page - 1) * $limit; // start index of placements to be displayed
+
+$rowCount = $placementsDataSet->fetchRowCountAll();
+
+$total = ceil($rowCount / $limit); // total number of pages
+
+
 view('/ViewAllPlacements/viewallplacements.phtml', [
         'pageTitle' => 'All Placements',
-        'allPlacements' => $placementsDataSet->fetchAllPlacements(),
+        'allPlacements' => $placementsDataSet->fetchByLimit($start, $limit),
         'companiesDataSet' => $companiesDataSet,
         'industriesDataSet' => $industriesDataSet,
         'skillsDataSet' => $skillsDataSet,
         'allProficiencies' => $proficienciesDataSet->fetchAllProficiencies(),
         'placementHelpers' => $placementHelpers,
+        'total' => $total,
+        'page' => $page,
+        'limit' => $limit
         ]
 );
