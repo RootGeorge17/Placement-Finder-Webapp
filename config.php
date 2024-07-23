e42d8cebffd30118f64d6a4325d10f5f09847492
George Sopov <84963057+RootGeorge17@users.noreply.github.com>
2024-07-23 16:44:20 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 return [
     'database' => [
-        'host' => 'poseidon.salford.ac.uk', // Database host
-        'dbname' => 'hc23_17_hackcamp', // Database name
+        'host' => '', // Database host
+        'dbname' => '', // Database name
         'charset' => 'utf8mb4' // Character set for the database connection
     ],
-    'username' => 'hc23-17',
-    'password' => 'LOanukKp2py35Mm'
+    'username' => '',
+    'password' => ''
 ];
